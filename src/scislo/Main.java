b573513134d571f6ab2c1d4883e95034dd46115b
Tomasz Ścisło <scislot@gmail.com>
2019-01-20 11:48:28 +0100

    Lang versions

--- src/scislo/Main.java
+++ src/scislo/Main.java
@@ -7,17 +7,22 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        fxmlLoader.setResources(ResourceBundle.getBundle("bundles.LangBundle", new Locale("en", "EN")));
         AnchorPane pane = fxmlLoader.load(); // To oznacza, że będziemy podpinać kontoler do AnchorPane
         primaryStage.setTitle("MVC1");
         primaryStage.setScene(new Scene(pane, 400, 300));
         primaryStage.setMinWidth(400);
         primaryStage.setMinHeight(300);
         MainWindowController mainWindowController = fxmlLoader.getController();
+        mainWindowController.setMain(this);
         primaryStage.show();
     }
 
--- src/scislo/MainWindowController.java
+++ src/scislo/MainWindowController.java
@@ -1,4 +1,41 @@
 package scislo;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class MainWindowController {
+
+    private Person person = new Person(1, "Karol", "Sztrazburger");
+
+    private Main main;
+
+    @FXML
+    private Button button;
+    @FXML
+    private TextField input;
+    @FXML
+    private Label label;
+
+
+    public MainWindowController() {
+    }
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    @FXML
+    public void handleButton() {
+        this.person.setFirstName(this.input.getText());
+        this.label.setText(this.person.getFirstName());
+    }
+
+
+
 }
